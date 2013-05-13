b7a1a6c6c31d87ee92584cc49982ef6343b47eb0
Étienne André <Andre.Etienne@lipn13.fr>
2013-05-13 11:48:00 +0000

    ajout build script pour IMPERATOR

---
+++ build.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+#################################################################
+ #
+ #                       BUILDATOR
+ # 
+ # Small script to build IMPERATOR
+ #
+ # Universite Paris 13, Sorbonne Paris Cite, LIPN (France)
+ # 
+ # Author:        Etienne Andre
+ # 
+ # Created:       2013/05/13
+ # Last modified: 2013/05/13
+ #
+################################################################
+ 
+set -e
+
+TARGET=IMPERATOR
+FLAGS="-libs str,nums,unix,/usr/lib/ocaml/extlib/extLib"
+OCAMLBUILD=ocamlbuild
+
+ocb()
+{
+  $OCAMLBUILD $FLAGS $*
+}
+
+rule() {
+  case $1 in
+    clean)  ocb -clean;;
+    native) ocb $TARGET.native;;
+    byte)   ocb $TARGET.byte;;
+    all)    ocb $TARGET.native $TARGET.byte;;
+    depend) echo "Not needed.";;
+    *)      echo "Unknown action $1";;
+  esac;
+}
+
+if [ $# -eq 0 ]; then
+  rule all
+else
+  while [ $# -gt 0 ]; do
+    rule $1;
+    shift
+  done
+fi
+
